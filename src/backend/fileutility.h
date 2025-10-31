835134ecdcee91ec66d7c6ffeeac05183996340f
qinterfly <qinterfly@gmail.com>
2025-10-31 17:17:23 +0700

    Sketch method for setting up a model

--- src/backend/fileutility.h
+++ src/backend/fileutility.h
@@ -27,6 +27,7 @@ namespace Backend::Utility
 {
 
 QSharedPointer<QFile> openFile(QString const& pathFile, QString const& expectedSuffix, QIODevice::OpenModeFlag const& mode);
+void setupModel(KCL::Model& model);
 
 //! Base case for combining a filepath
 template<typename T>
